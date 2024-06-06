095563cc7e0a35b05c71d942cdbcab97f2bf5ce0
Khawar Anees <khawaraneescp@gmail.com>
2024-06-06 18:01:21 +0500

    Policies pages template and converions done

--- assets/custom.js
+++ assets/custom.js
@@ -128,3 +128,299 @@
  * "photoswipe": v4.1.3. PhotoSwipe is only loaded on demand to power the zoom feature on product page. If the zoom
  * feature is disabled, then this script is never loaded.
  */
+
+jQuery("#featuredcarousel").owlCarousel({
+    autoplay: false,
+    loop: true,
+    /* use rewind if you don't want loop */
+    margin: 31,
+    /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 800,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 3
+        },
+
+        1024: {
+            items: 4
+        },
+
+        1366: {
+            items: 5.2
+        }
+    }
+});
+
+
+//   Featured Tabbing
+// Show the first tab by default
+$('.tabs-stage div').hide();
+$('.tabs-stage div:first').show();
+$('.tabs-nav li:first').addClass('tab-active');
+
+// Change tab class and dis<p>lay content
+$('.tabs-nav a').on('mouseover', function (event) {
+    event.preventDefault();
+    $('.tabs-nav li').removeClass('tab-active');
+    $(this).parent().addClass('tab-active');
+    $('.tabs-stage div').hide();
+    $($(this).attr('href')).show();
+});
+
+// Shop Product Tabbing
+
+const tabButtons = document.querySelectorAll('.tab-btn')
+
+tabButtons.forEach((tab) => {
+    tab.addEventListener('click', () => tabClicked(tab))
+})
+
+function tabClicked(tab) {
+
+    tabButtons.forEach(tab => {
+        tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+
+    const contents = document.querySelectorAll('.content')
+
+    contents.forEach((content) => {
+        content.classList.remove('show')
+    })
+
+    const contentId = tab.getAttribute('content-id')
+    const contentSelected = document.getElementById(contentId)
+
+    contentSelected.classList.add('show')
+    //console.log(contentId)
+}
+
+// Happy Customer Slider
+
+jQuery(document).ready(function() {
+    var owl = jQuery("#customercarousel");
+    var currentSlide = jQuery("#current-slide");
+    var totalSlides = jQuery("#total-slides");
+
+    owl.owlCarousel({
+        autoplay: true,
+        rewind: true, // use rewind if you don't want loop
+        responsiveClass: true,
+        autoHeight: true,
+        autoplayTimeout: 7000,
+        smartSpeed: 800,
+        nav: true,
+        navText: [
+            '<span class="slickPrev slide-arrow prev-arrow"><img src="images/left-arrow.svg" alt="arrow"></span>',
+            '<span class="slickNext slide-arrow next-arrow"><img src="images/right-arrow.svg" alt="arrow"></span>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1024: {
+                items: 1
+            },
+            1366: {
+                items: 1
+            }
+        },
+        onInitialized: updateCounter,
+        onTranslated: updateCounter
+    });
+
+    function updateCounter(event) {
+        // Total items
+        var itemCount = event.item.count;
+        totalSlides.text(itemCount);
+
+        // Current item index (note: event.item.index starts from 0)
+        var currentIndex = event.item.index + 1;
+        currentSlide.text(currentIndex);
+    }
+});
+
+
+
+//   About us slider
+
+$('.meet-team-prnt').owlCarousel({
+    loop: true,
+    margin: 32,
+    responsiveClass: true,
+    navigation: false,
+    center: true,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+        },
+        600: {
+            items: 3,
+            nav: false,
+            margin: 20,
+        },
+        1000: {
+            items: 3.5,
+            nav: false,
+            loop: true,
+            margin: 20,
+        },
+        1200: {
+            items: 4,
+            nav: false,
+            loop: true,
+        },
+        1440: {
+            items: 4.45,
+            nav: false,
+            loop: true,
+        }
+
+    }
+})
+
+// Contact Page Phone number
+
+document.addEventListener('DOMContentLoaded', () => {
+    const numberInput = document.getElementById('number');
+
+    numberInput.addEventListener('input', () => {
+        // Remove any non-numeric characters
+        numberInput.value = numberInput.value.replace(/[^0-9]/g, '');
+    });
+});
+
+
+// Hamburger
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+});
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $(this).toggleClass("is-active");
+        $('.menusflex').toggleClass('active');
+    });
+});
+// body Fix
+
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $("body").toggleClass("body-fix");
+    });
+});
+
+
+// Shop detail Thumbnail slider
+
+$(document).ready(function () {
+
+    var sync1 = $("#sync1");
+    var sync2 = $("#sync2");
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+
+    sync1.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: false,
+        autoplay: false,
+        dots: true,
+        loop: true,
+        responsiveRefreshRate: 200,
+        navText: [
+            '<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>',
+            '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'
+        ],
+    }).on('changed.owl.carousel', syncPosition);
+
+    sync2
+        .on('initialized.owl.carousel', function () {
+            sync2.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: true,
+            nav: false,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: 1, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100
+        }).on('changed.owl.carousel', syncPosition2);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync2
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = sync2.find('.owl-item.active').length - 1;
+        var start = sync2.find('.owl-item.active').first().index();
+        var end = sync2.find('.owl-item.active').last().index();
+
+        if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync2.on("click", ".owl-item", function (e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync1.data('owl.carousel').to(number, 300, true);
+    });
+});
